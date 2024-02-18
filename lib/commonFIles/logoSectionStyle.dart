53ce78701752bf0bc79b211d2922c593f866586b
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-18 17:31:41 +0530

    logoStyle

---
+++ lib/commonFIles/logoSectionStyle.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:home_services/constants/size_values.dart';
+
+class CTextStyle {
+  // Company name Style
+  static TextStyle companyTextStyle({
+    required Color color,
+    required double textSize,
+    required FontWeight textWeigth}){
+    return TextStyle(
+      color: color,
+      fontSize: textSize,
+      fontWeight: textWeigth,
+      letterSpacing: TSizeValues.companyLetterSpacing,
+      height: 1,
+    );
+  }
+}
