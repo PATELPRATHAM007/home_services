5656f3fef53e2c437664f171df91572dcd94b37a
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-22 18:13:40 +0530

    update login page

--- lib/login_Page.dart
+++ lib/login_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_services/anim/animation.dart';
 import 'package:home_services/commonFIles/logoSection.dart';
 import 'package:home_services/commonFIles/logoSectionStyle.dart';
 import 'package:home_services/constants/colors.dart';
@@ -6,6 +7,7 @@ import 'package:home_services/constants/image_strings.dart';
 import 'package:home_services/constants/size_values.dart';
 import 'package:home_services/constants/style.dart';
 import 'package:home_services/constants/text_strings.dart';
+import 'package:home_services/loginPageContent/textField.dart';
 
 class TLoginPage extends StatefulWidget {
   const TLoginPage({super.key});
@@ -16,7 +18,24 @@ class TLoginPage extends StatefulWidget {
 
 class _TLoginPageState extends State<TLoginPage> {
   final TextEditingController emailController = TextEditingController();
+
+  bool _showPassword = false;
   final TextEditingController passwordController = TextEditingController();
+  void passwordVisibility() {
+    setState(() {
+      _showPassword = !_showPassword;
+    });
+  }
+
+   bool isBlueContainerOnLeft = true;
+
+  void toggleContainerPosition() {
+    setState(() {
+      isBlueContainerOnLeft = !isBlueContainerOnLeft;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -40,200 +59,211 @@ class _TLoginPageState extends State<TLoginPage> {
                     // spreadRadius: 5
                   )
                 ]),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+            child: Stack(
               children: [
-                SizedBox(
-                  width: 900 * 0.55,
-                  height: 550,
-                  // color: Colors.blue,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CLogoSection(
-                              logoWidth: TSizeValues.logoWidth,
-                              logoHeight: TSizeValues.logoHeight,
-                              gap: TSizeValues.logoGap,
-                              textStyle: CTextStyle.companyTextStyle(
-                                  color: TColors.black,
-                                  textSize: TSizeValues.companyTextSize,
-                                  textWeigth: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Center(
-                        child: Text(
-                          "Welcome Back",
-                          style: TextStyle(
-                              letterSpacing: 2,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        height: 70,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 110),
-                          child: GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount:
-                                        3, // Number of items in a row
-                                    crossAxisSpacing: 10,
-                                    //  mainAxisSpacing: 8.0,
-                                    mainAxisExtent: 58),
-                            itemBuilder: (BuildContext context, int index) {
-                              // You can customize the content of each grid item (rectangle) here
-                              return Container(
-                                child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(20),
-                                            side: const BorderSide(
-                                                color: Colors.grey,
-                                                width: 1.5)),
-                                        backgroundColor: TColors.white),
-                                    child: const Icon(
-                                      Icons.alarm,
-                                      color: Colors.grey,
-                                    )),
-                              );
-                            },
-                            itemCount: 3, // Total number of items in the grid
+                Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 900 * 0.55,
+                    height: 550,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        // logo section
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CLogoSection(
+                                logoWidth: TSizeValues.logoWidth,
+                                logoHeight: TSizeValues.logoHeight,
+                                gap: TSizeValues.logoGap,
+                                textStyle: CTextStyle.companyTextStyle(
+                                    color: TColors.black,
+                                    textSize: TSizeValues.companyTextSize,
+                                    textWeigth: FontWeight.bold),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      Center(
-                        child: Text(
-                          "or use your email address for login",
-                          style: TextStyle(
-                              color: Colors.grey[400],
-                              fontSize: 16,
-                              letterSpacing: 1.3,
-                              fontWeight: FontWeight.w100),
+              
+                        // welcome text
+                        const Center(
+                          child: Text(
+                            "Welcome Back",
+                            style: TextStyle(
+                                letterSpacing: 2,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: TextField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            hintText: 'Enter your email',
-                            labelStyle: const TextStyle(color: Colors.grey),
-                            hintStyle: const TextStyle(color: Colors.grey),
-                            prefixIcon: const Icon(Icons.email_outlined,
-                                color: Colors.grey),
-                            filled: true,
-                            fillColor: Colors.white,
-                            contentPadding:
-                                const EdgeInsets.symmetric(horizontal: 35),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: const BorderSide(
-                                color: Color.fromARGB(255, 179, 49, 49),
+              
+                        //space
+                        const SizedBox(
+                          height: 20,
+                        ),
+              
+                        // login with google or other
+                        SizedBox(
+                          height: 70,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 110),
+                            child: GridView.builder(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount:
+                                          3, // Number of items in a row
+                                      crossAxisSpacing: 10,
+                                      //  mainAxisSpacing: 8.0,
+                                      mainAxisExtent: 58),
+                              itemBuilder: (BuildContext context, int index) {
+                                // You can customize the content of each grid item (rectangle) here
+                                return Container(
+                                  child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                          elevation: 0,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                              side: const BorderSide(
+                                                  color: Colors.grey,
+                                                  width: 1.5)),
+                                          backgroundColor: TColors.white),
+                                      child: const Icon(
+                                        Icons.alarm,
+                                        color: Colors.grey,
+                                      )),
+                                );
+                              },
+                              itemCount: 3, // Total number of items in the grid
+                            ),
+                          ),
+                        ),
+              
+                        // login breaker
+                        Center(
+                          child: Text(
+                            "or use your email address for login",
+                            style: TextStyle(
+                                color: Colors.grey[400],
+                                fontSize: 16,
+                                letterSpacing: 1.3,
+                                fontWeight: FontWeight.w100),
+                          ),
+                        ),
+              
+                        //space
+                        const SizedBox(
+                          height: 20,
+                        ),
+              
+                        // email field
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: LTextField(
+                              controller: emailController,
+                              lableText: "Email",
+                              hitText: "Enter your E-mail",
+                              perfixIcon: Icons.email_outlined,
+                              suffixIcon: null,
+                              obscureText: false),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+              
+                        // password field
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: LTextField(
+                            controller: passwordController,
+                            lableText: "Password",
+                            hitText: "Enter your password",
+                            perfixIcon: Icons.email_outlined,
+                            obscureText: _showPassword,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _showPassword
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: passwordVisibility,
+                            ),
+                          ),
+                        ),
+              
+                        // forgot password
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton( 
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Forgot Password",
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      decorationColor: TColors.blue,
+                                      fontSize: 16,
+                                      color: TColors.blue,
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ),
+              
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                          ),
+                          child: SizedBox(
+                            height: 50,
+                            width: 400,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 50, 88, 220)),
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                    color: TColors.white,
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w500),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: TextField(
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: const TextStyle(color: Colors.grey),
-                            hintStyle: const TextStyle(color: Colors.grey),
-                            hintText: 'Enter your password',
-                            prefixIcon: const Icon(
-                              Icons.key_rounded,
-                              color: Colors.grey,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            contentPadding:
-                                const EdgeInsets.symmetric(horizontal: 25),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: const BorderSide(
-                                color: Color.fromARGB(255, 179, 49, 49),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 25, vertical: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "Forgot Password",
-                                  style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    decorationColor: TColors.blue,
-                                    fontSize: 16,
-                                    color: TColors.blue,
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                        ),
-                        child: SizedBox(
-                          height: 50,
-                          width: 400,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 50, 88, 220)),
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                  color: TColors.white,
-                                  fontSize: 23,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  width: 900 * 0.45,
-                  height: 550,
-                  color: TColors.blue,
-                )
+                  Positioned(
+                        right: isBlueContainerOnLeft ? null : 0,
+                        left: isBlueContainerOnLeft ? 0 : null,
+                        child: Container(
+                          width: 900 * 0.45,
+                          height: 550,
+                          color: Colors.blue,
+                        ),
+                      ),                
+                ],
+              ),
               ],
+              
             ),
           ),
         ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child :FloatingActionButton(
+          onPressed: toggleContainerPosition,
+          child: Icon(Icons.arrow_forward),
+        ),
+        )
       ],
     ));
   }
