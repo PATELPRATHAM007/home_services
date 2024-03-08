a3814927e853371e8f1245de2d68c1c1d96087cf
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-03-08 14:06:54 +0530

    add_const_string

--- lib/constants/text_strings.dart
+++ lib/constants/text_strings.dart
@@ -10,4 +10,16 @@ class TTextStrings {
   static String aboutUs = "ANOUT US";
   static String contactUS = "CONTACT US";
 
+  // login page
+  static String signUp = 'Sign Up';
+  static String enterDetails = "Enter your detail and \n srart your journey with us";
+  static String hiiThere = "Hii there !!";
+  static String login = "Login";
+  static String forgotPass = "Forgot Password";
+  static String enterPass = "Enter your Password";
+  static String password = "Password";
+  static String enterEmail = "Enter your E-mail";
+  static String email = "Email";
+  static String loginOptaion = "or use your email address for login";
+  static String welcomeBack = "Welcome Back";
 }
