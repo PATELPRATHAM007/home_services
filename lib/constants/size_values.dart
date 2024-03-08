b9d4ea3883ead8d83449da3961f8437b8ceb1d73
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-03-08 14:06:40 +0530

    add_const_size

--- lib/constants/size_values.dart
+++ lib/constants/size_values.dart
@@ -25,4 +25,33 @@ class TSizeValues {
   static const double cityButtonBorder = 20;
 
 
-}
+  //login & sign up page 
+  static const double mianContWidth = 900;
+  static const double mianContHeight = 550;
+  static const double mianContRadious = 90;
+  static const double mianContShadowRadious = 90;
+
+  // sub container 1 of login & sign up page 
+  static const double subContWidth1 = mianContWidth * 0.55;
+  static const double subContheight1 = mianContHeight;
+
+  // sub container 2 of login & sign up page 
+  static const double subContWidth2 = mianContWidth * 0.45;
+  static const double subContheight2 = mianContHeight;
+
+  //login with google or other 
+  static const double girdBoxHeight = 70;
+  static const double girdBoxPadding = 110;
+  static const double girdBoxCrossSpace = 10;
+  static const double girdBoxMainExent = 58;
+  static const double girdBoxRadious = 20;
+  static const double girdBoxBorderWidth = 1.5;
+
+  // email field & other field
+  static const double fieldPadding = 40;
+
+  //login & signup button
+  static const double lsbtnHeight = 50;
+  static const double lsbtnwidth = 400;
+  
+} 
