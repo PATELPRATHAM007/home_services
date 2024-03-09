8b3e9f6f665b1c698f64b3ae1b159c0fec03b935
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-03-09 22:29:07 +0530

    navigate with login page

--- lib/commonFIles/navloginButton.dart
+++ lib/commonFIles/navloginButton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_services/login_Page.dart';
 
 class NavLoginButton extends StatefulWidget {
   const NavLoginButton({super.key});
@@ -11,7 +12,14 @@ class _NavLoginButtonState extends State<NavLoginButton> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TLoginPage(),
+                        ),
+                      );
+      },
       style: ElevatedButton.styleFrom(
         backgroundColor: Color.fromARGB(255, 50, 88, 220),
         minimumSize: const Size(100, 50),
